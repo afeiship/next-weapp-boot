143a83cf33f57f00d24423eacec335d17dd9b334
afeiship <1290657123@qq.com>
2018-08-22 15:38:30 +0800

    issue: add behavior

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     App: require('./lib/app'),
     Page: require('./lib/page'),
-    Component: require('./lib/component')
+    Component: require('./lib/component'),
+    Behavior: require('./lib/behavior')
 };
